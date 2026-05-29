26e56693aa062b0da857e6f3bd55fa60cd2f235b
lizhijie9413-art <lizhijie9413@gmail.com>
2026-05-29 15:20:23 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = "quantai-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
+  );
+});
